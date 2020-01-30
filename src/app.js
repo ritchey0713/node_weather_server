b58dfdcafee983103ae1de0e9f237fbd17703454
Dj <ritchey0713@gmail.com>
2020-01-30 12:57:06 -0500

    set up server

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get("", () => {
+app.get("", (req, resp) => {
+  
+})
 
+app.listen(3000, () => {
+  console.log('Server booted!')
 })
