96bb03b4045dd528e7a72944d2f10c70df83fb73
Dj <ritchey0713@gmail.com>
2020-01-30 14:17:35 -0500

    set up basic routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ app.get("", (req, resp) => {
   resp.send("test")
 })
 
+app.get("/help", (req, resp) => {
+  resp.send("Help page")
+})
+
+app.get("/about", (req, resp) => {
+  resp.send("about page")
+})
+
+app.get("/weather", (req, resp) => {
+  resp.send("weather")
+})
+
+
 app.listen(3000, () => {
   console.log('Server booted!')
 })
