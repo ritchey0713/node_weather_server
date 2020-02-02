6ebb5e28ce5438235adbfd3ad24e5a1c9969e945
Dj <ritchey0713@gmail.com>
2020-02-01 20:33:37 -0500

    add renders

--- src/app.js
+++ src/app.js
@@ -11,16 +11,27 @@ const app = express()
 app.set("view engine", "hbs")
 app.use(express.static(publicPath))
 
-// some nonsense stuff here
 
-
-app.get("/help", (req, resp) => {
-  
+app.get("", (req, resp) => {
+  resp.render('index', {
+    title: "weather",
+    name: "Dj"
+  })
 })
 
-// app.get("/about", (req, resp) => {
-//   resp.send("<h1>This is a help page</h1>")
-// })
+app.get("/help", (req, resp) => {
+  resp.render("help", {
+    title: "Help page",
+    content: "Please reach out with any issues"
+  })
+})
+
+app.get("/about", (req, resp) => {
+  resp.render("about", {
+    title: "About us",
+    name: "Dj"
+  })
+})
 
 app.get("/weather", (req, resp) => {
   resp.send({
