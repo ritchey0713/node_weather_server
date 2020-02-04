3952e07173535c92e9c3478e9538e7521c1afc3a
Dj <ritchey0713@gmail.com>
2020-02-04 13:37:44 -0500

    query serarch

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,18 @@
 console.log("hello")
 
-fetch("http://localhost:3000/weather?address=columbus")
-  .then((resp) => {
-   return resp.json()
-  }).then((data) => {
-    console.log(data)
-  }).catch((err) => {
-    console.log(err)
-  })
+// fetch("http://localhost:3000/weather?address=columbus")
+//   .then((resp) => {
+//    return resp.json()
+//   }).then((data) => {
+//     console.log(data)
+//   }).catch((err) => {
+//     console.log(err)
+//   })
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+})
