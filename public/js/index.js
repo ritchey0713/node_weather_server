12d5757906d7d17793caad1be6073535c0644fc1
Dj <ritchey0713@gmail.com>
2020-02-04 12:33:17 -0500

    end point test

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
 console.log("hello")
+
+fetch("http://localhost:3000/weather?address=columbus")
+  .then((resp) => {
+   return resp.json()
+  }).then((data) => {
+    console.log(data)
+  }).catch((err) => {
+    console.log(err)
+  })
