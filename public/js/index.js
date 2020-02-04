e80c00423ef589ce72139e18ff8fe7515ddc7a67
Dj <ritchey0713@gmail.com>
2020-02-04 13:49:33 -0500

    add async call

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,17 @@
-console.log("hello")
-
-// fetch("http://localhost:3000/weather?address=columbus")
-//   .then((resp) => {
-//    return resp.json()
-//   }).then((data) => {
-//     console.log(data)
-//   }).catch((err) => {
-//     console.log(err)
-//   })
+const baseURL = "http://localhost:3000/weather"
 
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 
+const fetchWeather = async(location) => {
+    let resp = await fetch(`${baseURL}?address=${location}`)
+    let data = await resp.json()
+    return data
+}
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault()
-
+  const location = search.value
+  fetchWeather(location)
+    .then(data => console.log(data))
 })
