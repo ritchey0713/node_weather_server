44d4a931a12c380d6e77a52be4dcdfa435b05256
Dj <ritchey0713@gmail.com>
2020-02-05 16:16:58 -0500

    deployment #1

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicPath = path.join(__dirname, "../public")
@@ -104,6 +105,6 @@ app.get("*", (req, resp) => {
 
 
 // port to use
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server booted!')
 })
